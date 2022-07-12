c3d5c1f74f1e0d80df4c0c081e214fd6f1008f13
Paulo de Tasso Oliveira de Lacerda <xcloudgameemail@gmail.com>
2022-07-11 22:20:05 -0300

    finish sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,16 +1,21 @@
+import { PencilLine } from 'phosphor-react';
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
       <img className={styles.cover} src="https://picsum.photos/500/300" alt="" />
+
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/p-lacerda.png" />
         <strong>Nome do usuário</strong>
         <span>Descrição</span>
       </div>
 
       <footer>
         <a href="#">
+          <PencilLine size={20} />
           Editar seu perfil
         </a>
       </footer>
