d1788286849ff97d0f92baa6c9803df51e40a41c
Paulo de Tasso Oliveira de Lacerda <xcloudgameemail@gmail.com>
2022-07-23 02:52:38 -0300

    feat: add more style to Post component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Header } from "./components/Header"
-import Post from "./Post"
+import { Post } from "./components/Post"
 import './global.css';
 import styles from './App.module.css';
 import { Sidebar } from "./components/Sidebar";
@@ -13,8 +13,8 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post author="J. R. R Tolkien" content="Senhor dos Aneis é muito bom" />
-          <Post author="Eu" content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur tempora sunt ad obcaecati rem nemo unde sint velit in. Quia magni consequuntur excepturi nobis distinctio sequi nesciunt, laboriosam ullam delectus!" />
+          <Post />
+          <Post />
         </main>
       </div>
     </div>
